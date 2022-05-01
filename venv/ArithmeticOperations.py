890ff4afc26ac35ed1d382ee3b98f983f8b7628d
TravieQueen <104170772+TravieQueen@users.noreply.github.com>
2022-05-01 16:56:28 -0400

    Added first two tutorial files.

---
+++ venv/ArithmeticOperations.py
@@ -0,0 +1,13 @@
+
+print(10+3)
+
+print(10/3) #divides into float
+print(10//3) #divides and rounds down
+print(10%3) #divides and displays remainder
+
+print(10**3) #power operator
+
+#incrememnting values
+print(x=10)
+x +=3
+print(x)
---
+++ venv/StringMethods.py
@@ -0,0 +1,14 @@
+
+
+course = 'Python for Beginners'
+
+print(len(course))
+print(course.upper())
+
+print(course.lower()) #string to lwoer
+
+print(course.find('P')) #finds index of char in string
+
+print(course.replace('Beginners', 'Absoulute Beginners')) #replaces variable
+
+print( 'Python' in course) #checks to see if value is in variable
